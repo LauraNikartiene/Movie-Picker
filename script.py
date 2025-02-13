9fdf1763a7858b2163df1b0ab3c2a0327774de33
LauraNikartiene <laura.kvasaite999@gmail.com>
2025-02-13 15:06:02 +0200

    Added script.py

--- script.py
+++ script.py
@@ -2,33 +2,63 @@ from data import *
 from welcome import *
 from linkedlist import LinkedList
 
-
 print welcome()
 
-#Code to insert movie types into data structure.
-
-def insert_movie_types():
-    movie_type_list = LinkedList()
-    for movie_type in types:
-        movie_type_list.insert_beginning(movie_type)
-    return movie_type_list
-
-#Code to insert movie data into a data structure.
+def insert_movie_genres():
+    movie_genre_list = LinkedList()
+    for movie_genre in genres:
+        movie_genre_list.insert_beginning(movie_genre)
+    return movie_genre_list
 
 def insert_movie_data():
     restaurant_data_list = LinkedList()
-    for movie_type in types:
+    for movie_genre in genres:
         movie_sublist = LinkedList()
         for movie in movie_data:
-            if movie[0] == movie_type:
+            if movie[0] == movie_genre:
                 movie_sublist.insert_beginning(movie)
         movie_data_list.insert_beginning(movie_sublist)
     return movie_data_list
 
-my_type_list = insert_movie_types()
+my_type_list = insert_movie_genres()
 my_movie_list = insert_movie_data()
 
-selected_movie_type = ""
+selected_movie_genre = ""
 
-#Code for user interaction.
+while len(selected_movie_genre) == 0:
+    user_input = str(input("\nWhat movie genre would you like to watch?\nType the beginning of that movie genre and press enter to see if it's here.\n")).lower()
 
+    matching_genres = []
+    genre_list_head = my_genre_list.get_head_node()
+    while genre_list_head is not None:
+        if str(genre_list_head.get_value()).startswith(user_input):
+            matching_genres.append(genre_list_head.get_value())
+
+    for genre in matching_genres:
+        print(genre)
+
+    if len(matching_genres) == 1:
+        select_genre = str(input("\nThe only matching genre for the specified input is " + matching_genres[0] + ". \nDo you want to look at " + matching_genres[0] + " movies? Enter y for yes and n for no\n")).lower()
+
+        if selected_genre == 'y':
+            selected_movie_genre = matching_types[0]
+            print("Selected movie genre: " + selected_movie_genre)
+            movie_list_head = my_movie_list.get_head_node()
+            while movie_list_head.get_next_node() is not None:
+                sublist_head = movie_list_head.get_value().get_head_node()
+                if sublist_head.get_value()[0] == selected_movie_genre:
+                    while sublist_head.get_next_node() is not None:
+                        print("----------------------------------------")
+                        print("Name: " + sublist_head.get_value()[1])
+                        print("Year: " + sublist_head.get_value()[2])
+                        print("Summary: " + sublist_head.get_value()[3])
+                        print("Rating: " + sublist_head.get_value()[4])
+                        print("----------------------------------------")
+                        sublist_head = sublist_head.get_next_node()
+                movie_list_head = movie_list_head.get_next_node()
+
+            repeat_loop = str(input("\nDo you want to find other movies? Enter y for yes and n for no.\n")).lower()
+            if repeat_loop == 'y':
+                select_movie_type = ""
+
+        
